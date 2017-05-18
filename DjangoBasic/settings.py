73668a724e79f3365713be6d2b77f36edfcbe33d
Pallav <pallav8parikh@gmail.com>
2017-05-18 13:44:18 +0530

    configuring for static files

--- DjangoBasic/settings.py
+++ DjangoBasic/settings.py
@@ -14,7 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 try:
